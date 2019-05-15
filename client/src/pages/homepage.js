266e3fc4e9a52138cd0fbdd5e8c1522585a02924
Jordan Choquette <j.w.choquette@gmail.com>
2019-05-15 07:38:51 -0600

    moved components into respective folder and started to get pages set up

---
+++ client/src/pages/homepage.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+import API from "../utils/API";
+import { Link } from "react-router-dom";
+import Navbar from "../components/Navbar";
+import Main from "../components/FrontMain";
+import CreateBtn from "../components/CreateButton";
+import JoinBtn from "../components/JoinButton";
+import Footer from "../components/Footer";
+
+class Homepage extends Component {
+    
+    render() {
+        return (
+            <div>
+                <h1>This is the Homepage</h1>
+            </div>
+        );
+    }
+}
+
+export default Homepage;
