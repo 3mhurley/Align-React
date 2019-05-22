f94e52b797ca50b693eef434c9673db5d8978a56
EH <33641969+3mhurley@users.noreply.github.com>
2019-05-21 20:24:12 -0600

    widget thingy beginning

---
+++ client/src/components/WidgetThingy/WidgetThingy.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+
+
+
+const styles = theme => ({
+
+  root: {
+    flexGrow: 1,
+    background: '#4794B3',
+    textAlign: 'center',
+    paddingTop: 207,
+    paddingBottom: 327,
+    marginTop: 3,
+    color: 'white',
+    overflow: 'hidden',
+    fontSize: 40
+
+  }
+});
+
+
+class WidgetThingy extends Component {
+  render() {
+    const { classes } = this.props;
+    return (
+      <body>
+        <div className={classes.root}>
+          <Grid container spacing={24}>
+            <Grid item xs={12}>
+              <h1>Align</h1>
+            </Grid>
+            <Grid item xs={6} className="createModal">
+              <CreateModal />
+
+            </Grid>
+            <Grid item xs={6} className="joinModal">
+              <JoinModal />
+
+            </Grid>
+          </Grid>
+        </div>
+      </body>
+
+    );
+
+  }
+}
+
+WidgetThingy.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(WidgetThingy);
