bbf9a0ac99f6718842522bf59ea4af4b1095c3d0
Jordan Choquette <j.w.choquette@gmail.com>
2019-05-21 21:39:46 -0600

    styling changes and added title button component with redirect to home

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,7 +1,6 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
-import { Redirect } from "react-router-dom";
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import TitleButton from '../TitleButton/TitleButton';
