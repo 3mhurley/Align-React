c6b00ba2abe54a1b54e2d2495fa47e23a3deabdf
EH <33641969+3mhurley@users.noreply.github.com>
2019-06-01 12:39:51 -0600

    fixing the JC

--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.44eb8983e0dae79b08781ee869a838de.js"
+  "/precache-manifest.b90d31bb276fadb67e6286d3f506a40f.js"
 );
 
 self.addEventListener('message', (event) => {
