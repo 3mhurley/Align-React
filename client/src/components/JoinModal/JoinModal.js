1669314ecc94dbd023085f645074f61c97b1913b
Anne Wilson <annewilson2591@gmail.com>
2019-05-31 20:06:03 -0600

    join modal routing

--- client/src/components/JoinModal/JoinModal.js
+++ client/src/components/JoinModal/JoinModal.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
 import Dialog from '@material-ui/core/Dialog';
@@ -7,20 +8,43 @@ import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import auth0Client from '../../Auth';
+import API from '../../utils/API';
 import "./joinmodal.scss"
 
 class JoinModal extends React.Component {
     state = {
         open: false,
+        join: {
+            calendarId: '',
+            userId: '',
+        }
     };
 
     handleClickOpen = () => {
         this.setState({ open: true });
     };
 
+    handleInputChange = (name, value) => {
+        const join = { ...this.state.join };
+        join[name] = value;
+        this.setState({ join });
+    }
+
     handleClose = () => {
         this.setState({ open: false });
     };
+
+    //on submit send payload to db
+    handleSubmit = () => {
+        const payload = this.state.join
+        API.createUser(payload)
+            .then(response => {
+                console.log(response);
+                //reroute to calendar on submit
+                this.props.history.push('/calendar');
+            })
+            .catch(error => console.error(error));
+    }
 
     render() {
         return (
@@ -43,20 +67,28 @@ class JoinModal extends React.Component {
                             If you have been invited to join a calendar enter your information.
             </DialogContentText>
                         <TextField
+                            onChange={ e => this.handleInputChange(e.target.name, e.target.value) }
+                            name="calendarId"
+                            value={ this.state.join.userId }
                             margin='dense'
                             id='cal-id'
                             label='Calendar ID'
                             type='string'
                             fullWidth
+                            InputLabelProps={{
+								shrink: true,
+							}}
                         />
                         <TextField
                             margin='dense'
                             id='guest-name'
                             label='Guest Name'
                             type='string'
-                            fullWidth
                         />
                         <TextField
+                            onChange={ e => this.handleInputChange(e.target.name, e.target.value) }
+                            name="userId"
+                            value={ this.state.join.userId }
                             margin="dense"
                             id="name"
                             label="Email Address"
@@ -69,10 +101,11 @@ class JoinModal extends React.Component {
                             Cancel
             </Button>
             {
-                !auth0Client.isAuthenticated() &&
-                <Button onClick={auth0Client.signIn} color="primary">
-                Submit
-                </Button>
+                auth0Client.isAuthenticated()
+                ?	<Button onClick={this.handleSubmit}>Submit</Button>
+                :	<Button onClick={ () => this.handleSubmit()} color='primary'>
+                        Log In and Submit
+                    </Button>
             }
                     </DialogActions>
                 </Dialog>
