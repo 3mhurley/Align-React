427230b8b0ad6e577a9750639c87a8669aaa2f15
Jordan Choquette <j.w.choquette@gmail.com>
2019-05-31 22:56:07 -0600

    add logo component and clean up code

---
+++ client/src/components/Logo/Logo.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import logo from './logo.png';
+import './logo.scss';
+
+console.log(logo);
+
+function HeaderImg() {
+    return <img src={logo} alt="Logo" id="alignLogo" />;
+}
+
+export default HeaderImg;
